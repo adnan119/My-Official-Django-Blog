3b91e6d98bef22edb05b7cd59792c01bf32a046a
adnan119 <adnanpen119@gmail.com>
2018-02-25 18:12:47 +0530

    make changes

--- myblog/settings.py
+++ myblog/settings.py
@@ -78,14 +78,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myblog',
-        'USER':'adnanpen',
-        'PASSWORD':'',
-        'HOST': 'localhost',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DEBUG = True
 
 
 # Password validation
@@ -130,6 +128,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
